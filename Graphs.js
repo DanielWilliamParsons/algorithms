22a3ac62dbe6315127a521f3f4775a00c72f6a85
Daniel William Parsons <parsonsdwp@gmail.com>
2024-09-05 14:57:10 +0900

    Breadth first traverse with queues and nodes classes

--- Graphs.js
+++ Graphs.js
@@ -2,11 +2,82 @@
  * Graphs, object-orientated implementation
  */
 
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.nextNode;
+    }
+
+    getNode() {
+        return this.value;
+    }
+
+    getNextNode() {
+        return this.nextNode;
+    }
+
+    setNextNode(aNode) {
+        this.nextNode = aNode;
+    }
+}
+
+class Queue {
+    firstItem;
+    constructor() {
+        
+    }
+
+    getLastItem(aNode) {
+        if(aNode.getNextNode() === undefined){
+            return aNode;
+        } else {
+            let newNode = aNode.getNextNode();
+            return this.getLastItem(newNode);
+        }
+    }
+
+    enqueue(item) {
+        let aNode = new Node(item);
+        if (!this.firstItem) {
+            this.firstItem = aNode;
+        } else {
+            this.getLastItem(this.firstItem).setNextNode(aNode);
+        }
+    }
+
+    dequeue() {
+        if(this.firstItem === undefined) {
+            return undefined;
+        }
+        let itemToReturn = this.firstItem;
+        this.firstItem = this.firstItem.getNextNode();
+        return itemToReturn.getNode();
+    }
+
+    isEmpty() {
+        if (this.firstItem === undefined) {
+            return true;
+        }
+        return false;
+    }
+
+    readAllItems(allItems = [], anItem = this.firstItem) {
+        if(this.firstItem !== undefined) {
+            allItems.push(anItem.getNode());
+            if (anItem.getNextNode() !== undefined) {
+                return this.readAllItems(allItems, anItem.getNextNode());
+            }
+        }
+        return allItems;
+    }
+}
+
 class Vertex {
     constructor(value) {
         this.value = value;
         this.adjacentVertices = [];
         this.searchTracker = new Map();
+        this.queueMaker;
     }
 
     // Directed graph
@@ -23,6 +94,10 @@ class Vertex {
         console.log("Adding", vertex.getValue(), "to", this.getValue());
         this.adjacentVertices.push(vertex);
         vertex.addAdjacentVertex(this);
+    }
+
+    getAdjacentVertices() {
+        return this.adjacentVertices;
     }
 
     getValue() {
@@ -67,11 +142,39 @@ class Vertex {
                 if (adjacentVertex.getValue() === searchValue) {
                     return adjacentVertex;
                 } else {
-                    adjacentVertex.depthFirstSearch(searchValue, tracker);
+                    return adjacentVertex.depthFirstSearch(searchValue, tracker);
                 } 
             }
         }
+    }
 
+    /**
+     * Breadth-First Search
+     * 1. Start at any vertex - startingVertex
+     * 2. Add starting vertex to tracker hash table.
+     * 3. Add starting vertex to a queue.
+     * 4. Start a loop that runs while the queue isn't empty
+     * 5. Within this loop, remove the first vertex from the queue - currentVertex
+     * 6. Iterate over all adjacent vertices of current vertex
+     * 7. If adjacent vertex was already visited, ignore it.
+     * 8. It the adjacent vertex has not yet been visited, mark it as visited
+     *    by adding it to the hash table and add it to the queue
+     * 9. Repeat this loop (from step 4) until queue is empty.
+     */
+
+    traverseVertices2(queue = new Queue(), tracker = this.searchTracker) {
+        queue.enqueue(this);
+        tracker.set(this.getValue(), true);
+        while (!queue.isEmpty()) {
+            let vertex = queue.dequeue();
+            for (let adjacentVertex of vertex.getAdjacentVertices()) {
+                if (!tracker.has(adjacentVertex.getValue())) {
+                    queue.enqueue(adjacentVertex);
+                    tracker.set(adjacentVertex.getValue(), true);
+                }
+            }
+        }
+        return tracker;
     }
 
      
@@ -89,6 +192,16 @@ bob.addAdjacentVertex(cynthia);
 cynthia.addAdjacentVertex(jane);
 bob.addAdjacentVertex(dominic);
 jane.addAdjacentVertex(dominic);
-//console.log(alice.traverseVertices());
-console.log(bob.depthFirstSearch("dominic"))
+// console.log(alice.traverseVertices());
+// console.log(alice.depthFirstSearch("dominic"));
 // console.log(dominic.traverseVertices());
+console.log(dominic.traverseVertices2());
+
+// let myQ = new Queue();
+// myQ.enqueue(alice);
+// myQ.enqueue(bob);
+// console.log(myQ);
+// console.log(myQ.dequeue());
+// console.log(myQ.readAllItems());
+// console.log(myQ.dequeue());
+// console.log(myQ.dequeue());
